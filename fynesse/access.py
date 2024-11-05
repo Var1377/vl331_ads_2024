3c05f8cc257c04f4c33b9c525f4d808263839f79
Var1377 <vl331@cam.ac.uk>
2024-11-05 15:22:58 +0000

    download price paid data

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,4 +1,5 @@
 from .config import *
+import requests
 
 """These are the types of import we might expect in this file
 import httplib2
@@ -11,7 +12,22 @@ import sqlite"""
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
+def download_price_paid_data(year_from, year_to):
+    # Base URL where the dataset is stored 
+    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+    """Download UK house price data for given year range"""
+    # File name with placeholders
+    file_name = "/pp-<year>-part<part>.csv"
+    for year in range(year_from, (year_to+1)):
+        print (f"Downloading data for year: {year}")
+        for part in range(1,3):
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                    file.write(response.content)
+
+
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
-
+    download_price_paid_data(2011, 2020)
